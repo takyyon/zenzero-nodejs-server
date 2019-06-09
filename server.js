5d607d7f63cf3b0bda33b0176fc8357f0c0de733
Krrish Mittal <takyyon@Krrishs-MacBook-Pro.local>
2019-06-09 14:31:43 -0400

    cors allowed

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const yelp = require('yelp-fusion');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const yelpService = require('./services/yelp.service.server');
 yelpService(app, yelp);
