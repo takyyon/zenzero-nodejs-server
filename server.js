00720e52eee58616fe39aef7c5dba72cedd8631e
Krrish Mittal <takyyon@Krrishs-MacBook-Pro.local>
2019-06-09 00:03:23 -0400

    yelp fusion setup

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const yelp = require('yelp-fusion');
+
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
