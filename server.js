902c6f7b92c5cfb87cbc894504148a2355d18a79
Krrish Mittal <takyyon@Krrishs-MacBook-Pro.local>
2019-06-19 22:32:42 -0400

    mongoose setup

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const yelp = require('yelp-fusion');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
--- utility/constants.js
+++ utility/constants.js
@@ -1,8 +1,10 @@
 module.exports = () => {
     const yelpApiKey = 'wnEY3d9XBecpESB6KahUY26Q6VNVWszwLgdV_DY4k-iwZA2ANQJ56vmX8pdoeGvN6WqQ7JDk9KzwSEErmlcH9sxMDU9l8ZkNkHhDXsg4DpZle9GCM_j7jd2XEYf8XHYx';
     const yelpClientKey = 'GC0pNqv02girBsMTwJoOvA';
+    const mongodbString = 'mongodb://heroku_t3g5sf53:urs6qkj1l4k813sv486f6d4iq0@ds241097.mlab.com:41097/heroku_t3g5sf53';
     return {
         yelpApiKey,
-        yelpClientKey
+        yelpClientKey,
+        mongodbString
     }
 };
